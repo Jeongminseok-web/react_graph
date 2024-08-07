52501980abcd83a37e283bf4de3df04ef6f6c8cc
jeongminseok <jeong010328@gmail.com>
2024-08-07 18:12:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Appbar from './components/baselayout/Appbar';
+import Sidebar from './components/baselayout/Sidebar';
+import DashboardScreen from './components/dashboard/DashboardScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full bg-[#212121] text-white">
+      <div className="page-wrapper h-screen flex">
+        <Sidebar />
+        <div className="content-wrapper">
+          <Appbar />
+          <DashboardScreen />
+        </div>
+      </div>
     </div>
   );
 }
